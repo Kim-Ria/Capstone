4f51a24ad92c9d0e9d78d3c9f8a2b32184300a99
Kim-Ria <104482046+Kim-Ria@users.noreply.github.com>
2022-05-03 21:11:19 +0900

    [modify] alert 추가

--- Run On/assets/js/custom.js
+++ Run On/assets/js/custom.js
@@ -1,40 +1,113 @@
+/** reserv_detail.html */
+/** 예약 상세 조회 페이지 alert */
 // 예약 취소 alert
 function cancelRev(){
-    swal({
-      title: "예약 취소",
-      text: "예약을 취소하시겠습니까?",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((cancel) => {
-      if (cancel) {
-        swal("예약이 취소되었습니다.", {
-          icon: "success",
-        }).then(() => { location.href = './reserv_history.html'; })
-      }
-    });
-  }
+  swal({
+    title: "예약 취소",
+    text: "예약을 취소하시겠습니까?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((cancel) => {
+    if (cancel) {
+      swal("예약이 취소되었습니다.", {
+        icon: "success",
+      }).then(() => { location.href = './reserv_history.html'; })
+    }
+  });
+}
 
-  // 퇴실 alert
-  function checkOut(){
-    swal({
-      title: "이용 완료",
-      text: "정말 퇴실하시겠습니까?",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((cancel) => {
-      if (cancel) {
-        swal("퇴실 처리 완료되었습니다.", {
-          icon: "success",
-        }).then(() => { location.href = './main.html'; })
-      }
-    });
-  }
+// 퇴실 alert
+function checkOut(){
+  swal({
+    title: "이용 완료",
+    text: "정말 퇴실하시겠습니까?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((cancel) => {
+    if (cancel) {
+      swal("퇴실 처리 완료되었습니다.", {
+        icon: "success",
+      }).then(() => { location.href = './main.html'; })
+    }
+  });
+}
 
-  // 예약 연장 alert
-  function extendRev(){
-    swal("예약 연장", "이용 시간이 2시간 연장되었습니다.", "success");
-  }
+// 예약 연장 alert
+function extendRev(){
+  swal("예약 연장", "이용 시간이 2시간 연장되었습니다.", "success");
+}
+
+
+
+/** mypage.html */
+/** 마이페이지 통계 */
+// 일주일 통계
+function statWeek(){
+  document.getElementById("statW").className = "btn active";
+  document.getElementById("statM").className = "btn btn-outline-primary";
+  document.getElementById("statY").className = "btn btn-outline-primary";
+
+  document.getElementById("useNo").innerText = "1회";
+  document.getElementById("useNoPer").innerText = "1";
+  document.getElementById("useNoDir").className = "fas fa-caret-up";
+
+  document.getElementById("useTime").innerText = "50분";
+  document.getElementById("useTimePer").innerText = "11";
+  document.getElementById("useTimeDir").className = "fas fa-caret-down";
+}
+
+// 한달 통계
+function statMonth(){
+  document.getElementById("statW").className = "btn btn-outline-primary";
+  document.getElementById("statM").className = "btn active";
+  document.getElementById("statY").className = "btn btn-outline-primary";
+
+  document.getElementById("useNo").innerText = "3회";
+  document.getElementById("useNoPer").innerText = "2";
+  document.getElementById("useNoDir").className = "fas fa-caret-down";
+
+  document.getElementById("useTime").innerText = "1시간 10분";
+  document.getElementById("useTimePer").innerText = "22";
+  document.getElementById("useTimeDir").className = "fas fa-caret-down";
+}
+
+// 1년 통계
+function statYear(){
+  document.getElementById("statW").className = "btn btn-outline-primary";
+  document.getElementById("statM").className = "btn btn-outline-primary";
+  document.getElementById("statY").className = "btn active";
+
+  document.getElementById("useNo").innerText = "5회";
+  document.getElementById("useNoPer").innerText = "3";
+  document.getElementById("useNoDir").className = "fas fa-caret-down";
+
+  document.getElementById("useTime").innerText = "3시간 30분";
+  document.getElementById("useTimePer").innerText = "33";
+  document.getElementById("useTimeDir").className = "fas fa-caret-up";
+}
+
+
+
+
+/** adminMember.html */
+/** 블랙리스트 추가 alert */
+function addBlacklistAdmin(){
+  swal({
+    title: "블랙리스트",
+    text: "등록하시겠습니까?",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((cancel) => {
+    if (cancel) {
+      swal("블랙리스트에 등록되었습니다.", {
+        icon: "success",
+      }).then(() => { location.href = './adminMember.html'; })
+    }
+  });
+}
